c0ad5f6f967e4cdc53fd578031977dc7a23fa363
Deep Thakkar <deepthakkardash24@gmail.com>
2025-12-15 17:17:18 +0530

    Adding core project of jar file

---
+++ src/main/java/com/cooknest/cooknest/CooknestApplication.java
@@ -0,0 +1,13 @@
+package com.cooknest.cooknest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooknestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooknestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cooknest/cooknest/CooknestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooknest.cooknest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooknestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
